395f4dc765ccd8686ef2053afb1c4f10920be61f
Edgar Torres <torres.edgar@gmail.com>
2020-05-18 22:26:13 -0600

    End of section 6

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'http://localhost:5000/api/auth/'
+  baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+
 constructor(private http: HttpClient) { }
 
   login(model: any) {
@@ -16,6 +21,7 @@ constructor(private http: HttpClient) { }
           const user = response;
           if (user){
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
           }
         })
       );
@@ -24,4 +30,11 @@ constructor(private http: HttpClient) { }
   register(model: any){
     return this.http.post(this.baseUrl + 'register', model);
   }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    const isLoggedIn = this.jwtHelper.isTokenExpired(token) ? false : true;
+    return token === null ? false : isLoggedIn;
+  }
+
 }
